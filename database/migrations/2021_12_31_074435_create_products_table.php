ea2a9dd19f45a97f3ce19e0ba6161d370e61cb3b
Kevinjn0123 <kevinjn0123@gmail.com>
2021-12-31 15:45:57 +0530

    new nova category not working

--- database/migrations/2021_12_31_074435_create_products_table.php
+++ database/migrations/2021_12_31_074435_create_products_table.php
@@ -15,6 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('image_path')->unique();
             $table->string('name');
             $table->text('description')->nullable();
             $table->unsignedInteger('quantity')->default(1);
